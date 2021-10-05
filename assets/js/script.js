b8223486652162e36b1082f8f29350144f7fad32
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2021-10-05 23:04:05 +0200

    Delete unnecessary code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,3 @@
-const txt = `"1","Ogrodzieniec","Zamek Ogrodzieniec – ruiny zamku leżącego na Jurze Krakowsko-Częstochowskiej, wybudowanego w systemie tzw. Orlich Gniazd, we wsi Podzamcze w województwie śląskim, w powiecie zawierciańskim, około 2 km na wschód od Ogrodzieńca. Zamek został wybudowany w XIV – XV w. przez ród Włodków Sulimczyków.","99PLN","50PLN"
-"2","Ojców","wieś w województwie małopolskim, w powiecie krakowskim, w gminie Skała, na terenie Wyżyny Krakowsko-Częstochowskiej, w Dolinie Prądnika, na Szlaku Orlich Gniazd. W Królestwie Polskim istniała gmina Ojców. W latach 1975–1998 miejscowość położona była w województwie krakowskim. W latach 1928–1966 Ojców miał status uzdrowiska posiadającego charakter użyteczności publicznej.","40PLN","15PLN`;
-
-// console.log(txt.split(/[\r\n]+/gm));
-
 const fileEl = document.querySelector('.uploader__input');
 fileEl.addEventListener('change', readFile);
 const ulTours = document.querySelector('.panel__excursions');
@@ -138,7 +133,6 @@ function firstTourOrder(
 		}
 		console.log(basketOrder);
 
-		// totalValue
 		totalValueSummary(basketOrder);
 	}
 }
@@ -182,7 +176,6 @@ function secondTourOrder(
 		}
 		console.log(basketOrder);
 
-		// totalValue
 		totalValueSummary(basketOrder);
 	}
 }
